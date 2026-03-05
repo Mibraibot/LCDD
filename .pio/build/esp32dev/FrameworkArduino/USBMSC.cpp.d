85a293c9b31c0ad992efa15cff52749f4581b054
ibot <happyhomekost@gmail.com>
2026-03-05 23:18:45 +0700

    Initial commit

---
+++ .pio/build/esp32dev/FrameworkArduino/USBMSC.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32dev/FrameworkArduino/USBMSC.cpp.o: \
+ /Users/bayuish/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.cpp \
+ /Users/bayuish/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
+ /Users/bayuish/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
